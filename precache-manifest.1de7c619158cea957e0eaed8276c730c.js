294e82be77495df2ce56b3b8849584dbf546a13c
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-12-17 10:15:55 +0530

    Updates

---
+++ precache-manifest.1de7c619158cea957e0eaed8276c730c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab79478185c90c8e145ac1fe80ca04d",
+    "url": "/UI-implementation/index.html"
+  },
+  {
+    "revision": "c354b72701a8759267e8",
+    "url": "/UI-implementation/static/css/main.c4bfc018.chunk.css"
+  },
+  {
+    "revision": "2c16a04a89967de365b9",
+    "url": "/UI-implementation/static/js/2.a6501b75.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/UI-implementation/static/js/2.a6501b75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c354b72701a8759267e8",
+    "url": "/UI-implementation/static/js/main.c084f004.chunk.js"
+  },
+  {
+    "revision": "f7a8f241b9f0d3cdaf1a",
+    "url": "/UI-implementation/static/js/runtime-main.8c758f92.js"
+  }
+]);
