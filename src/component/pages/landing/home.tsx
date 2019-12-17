6152c3b1faeb129544cbbfab71259ba2f4f8dc63
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-12-17 15:54:18 +0530

    use array of objects for render data

--- src/component/pages/landing/home.tsx
+++ src/component/pages/landing/home.tsx
@@ -5,96 +5,26 @@ import icon from "../../layout/icon"
 
 
 
-interface WelcomeProps {
-    uses?: string, // Change the required prop to an optional prop.
-    implements?: string,
-    openSource?: string,
-    session?: string,
-    complexity?: string,
-    scraping?: string
-}
-
-
-const Welcome: React.FC<WelcomeProps> = (props) => {
-
-
-    return (<div>
-        <div className="home-container">
+const Welcome = () => {
+    return (
+        <div className="home-container" >
             <div className="flex-container">
-                <div className="boxflex">
-                    <div className="icon">
-                        {icon({ name: "easeOfUse.png" })}
+                {data.map(item => (
+                    console.log(item),
+                    <div className="boxflex">
+                        <div className="icon">
+                            {icon({ name: item.img })}
+                        </div>
+                        <div className="multi-box" key={item.name}>
+                            <h3>{item.name}</h3>
+                            <h4>{item.value}</h4>
+                        </div>
                     </div>
-                    <div className="multi-box">
-                        <h3> Easy to use and understand</h3>
-                        <h4>{data.uses}</h4>
-                    </div>
-                </div>
-                <div className="vertical-box" style={{ backgroundImage: "linear-gradient(to bottom, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4" }}></div>
-                <div className="boxflex">
-                    <div className="icon">
-                        {icon({ name: "openSource.png" })}
-                    </div>
-                    <div className="multi-box">
-                        <h3> Open Source</h3>
-                        <h4>{data.openSource}</h4>
-                    </div>
-                </div>
+                ))}
+                <div className="box" style={{ backgroundImage: "linear-gradient(to bottom, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4" }}></div>
             </div>
+        </div >)
+};
 
-            <div className="box" style={{ backgroundImage: "linear-gradient(to right, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4" }}></div>
-
-            <div className="flex-container">
-                <div className="boxflex">
-                    <div className="icon">
-                        {icon({ name: "endToEnd.png" })}
-                    </div>
-                    <div className="multi-box">
-                        <h3> End-to-end implementation</h3>
-                        <h4>{data.implementation}</h4>
-                    </div>
-                </div>
-                <div className="vertical-box" style={{ backgroundImage: "linear-gradient(to bottom, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4" }}></div>
-                <div className="boxflex">
-                    <div className="icon">
-                        {icon({ name: "longSession.png" })}
-                    </div>
-                    <div className="multi-box">
-                        <h3> Long lived sessions</h3>
-                        <h4>{data.session}</h4>
-                    </div>
-                </div>
-
-            </div>
-            <div className="box" style={{ backgroundImage: "linear-gradient(to right, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4;" }}></div>
-            <div className="flex-container">
-                <div className="boxflex">
-                    <div className="icon">
-                        {icon({ name: "spaceComplex.png" })}
-                    </div>
-                    <div className="multi-box">
-                        <h3> Optimal Time and Space Complexity</h3>
-                        <h4>{data.complexity}</h4>
-                    </div>
-                </div>
-                <div className="vertical-box" style={{ backgroundImage: "linear-gradient(to bottom, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4" }}></div>
-                <div className="boxflex">
-                    <div className="icon">
-                        {icon({ name: "scraping.png" })}
-                    </div>
-                    <div className="multi-box">
-                        <h3> Prevents unwanted data scraping.</h3>
-                        <h4>{data.scraping}</h4>
-                    </div>
-                </div>
-            </div>
-            <div className="box" style={{ backgroundImage: "linear-gradient(to right, rgb(34, 34, 34), rgb(85, 85, 85), rgb(34, 34, 34))", opacity: "0.4;" }}></div>
-        </div>
-    </div >);
-}
-Welcome.defaultProps = {
-    uses: data.uses, // This value is adopted when name prop is omitted.
-    implements: data.implementation
-}
 
 export default Welcome;
--- src/configs/contentConfig.js
+++ src/configs/contentConfig.js
@@ -1,9 +1,34 @@
-const data = {
-    uses: "Clean and intuitive set of functions/APIs.",
-    openSource: "Good documentation and discounted dedicated support for early adopters.",
-    implementation: "Manages everything, from cookies to the database, along with multi process synchronisation.",
-    session: "We provide the best and most secure solution for all use cases. Even the ones where you require your users to be logged in for an extended period of time.",
-    complexity: "Depending on your setup, we store just one set of auth tokens per user per device. Also, most authentication requests don’t need a database call.",
-    scraping: "Our session management solution makes it much harder to systematically scrape your data."
-}
+const data = [
+    {
+        img: "easeOfUse.png",
+        name: "Easy to use and understand",
+        value: "Clean and intuitive set of functions/APIs.",
+    },
+    {
+        img: "openSource.png",
+        name: "Open source",
+        value: "Good documentation and discounted dedicated support for early adopters.",
+    },
+    {
+        img: "endToEnd.png",
+        name: "End-to-end implementation",
+        value: "Manages everything, from cookies to the database, along with multi process synchronisation.",
+    },
+    {
+        img: "longSession.png",
+        name: "Long lived sessions",
+        value: "We provide the best and most secure solution for all use cases. Even the ones where you require your users to be logged in for an extended period of time.",
+    },
+    {
+        img: "spaceComplex.png",
+        name: "Optimal Time and Space Complexity",
+        value: "Depending on your setup, we store just one set of auth tokens per user per device. Also, most authentication requests don’t need a database call.",
+    },
+    {
+        img: "scraping.png",
+        name: "Prevents unwanted data scraping.",
+        value: "Our session management solution makes it much harder to systematically scrape your data."
+    }
+
+]
 export default data
